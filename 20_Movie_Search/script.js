41e7f2e950fd18c3ef747296055915b0648d27dc
Manuacharya55 <101309561+Manuacharya55@users.noreply.github.com>
2024-03-17 11:18:46 +0530

    Update script.js

--- 20_Movie_Search/script.js
+++ 20_Movie_Search/script.js
@@ -9,7 +9,7 @@ search.addEventListener('click', (e) => {
 async function fetchMovie(moviename) {
     const data = await fetch(`https://www.omdbapi.com/?t=${moviename}&apikey=2e0f4086`);
     const { Title, Year, Genre, Plot, Language, Poster, imdbRating } = await data.json()
-    if (data.Title == "undefined") {
+    if (data.Title !== "undefined") {
         bindData(Title, Year, Genre, Plot, Language, Poster, imdbRating);
     } else {
         alert("No Movie Found in the Database we will let you know when it arrives");
